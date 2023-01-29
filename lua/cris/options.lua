8709a39568e6aadc45487ca771cfcbcdc162746b
crpier <crpier42@gmail.com>
2023-01-30 01:24:15 +0200

    lua only autocomand for lastplace

--- lua/cris/options.lua
+++ lua/cris/options.lua
@@ -57,10 +57,16 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- misc autocommands
 local misc_group = vim.api.nvim_create_augroup("Misc", { clear = true })
 -- Return to last cursor location
-vim.api.nvim_create_autocmd(
-  "BufReadPost",
-  { command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]], group = misc_group }
-)
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+  group = misc_group,
+})
 -- Easy quit with q on certain buffers
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "qf", "help", "man", "lspinfo" },
